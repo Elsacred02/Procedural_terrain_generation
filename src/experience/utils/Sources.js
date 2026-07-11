c6c28384f2a3bd10578095506dd85a92e20ab9a2
Marco Ferioli <m.ferioli@campus.unimib.it>
2026-07-11 15:49:42 +0200

    Effettuato il caricamento delle risorse

---
+++ src/experience/utils/Sources.js
@@ -0,0 +1,97 @@
+export default [
+    {
+        name : 'Cloud',
+        type : 'gltfModel',
+        path : ['./models/clouds/cloud.glb']
+    },
+
+    {
+        name : 'House1',
+        type : 'gltfModel',
+        path : ['./models/houses/house1.glb']
+    },
+
+    {
+        name : 'House2',
+        type : 'gltfModel',
+        path : ['./models/houses/house2.glb']
+    },
+
+    {
+        name : 'House3',
+        type : 'gltfModel',
+        path : ['./models/houses/house3.glb']
+    },
+
+    {
+        name : 'House4',
+        type : 'gltfModel',
+        path : ['./models/houses/house4.glb']
+    },
+
+    {
+        name : 'House5',
+        type : 'gltfModel',
+        path : ['./models/houses/house5.glb']
+    },
+
+    {
+        name : 'Rock1',
+        type : 'gltfModel',
+        path : ['./models/rocks/rock1.glb']
+    },
+
+    {
+        name : 'Rock2',
+        type : 'gltfModel',
+        path : ['./models/rocks/rock2.glb']
+    },
+
+    {
+        name : 'Rock3',
+        type : 'gltfModel',
+        path : ['./models/rocks/rock3.glb']
+    },
+
+    {
+        name : 'Rock4',
+        type : 'gltfModel',
+        path : ['./models/rocks/rock4.glb']
+    },
+
+    {
+        name : 'Rock5',
+        type : 'gltfModel',
+        path : ['./models/rocks/rock5.glb']
+    },
+
+    {
+        name : 'BigOak',
+        type : 'gltfModel',
+        path : ['./models/trees/big_oak.glb']
+    },
+
+    {
+        name : 'Birch',
+        type : 'gltfModel',
+        path : ['./models/trees/birch.glb']
+    },
+
+    {
+        name : 'DeadTree',
+        type : 'gltfModel',
+        path : ['./models/trees/dead_tree.glb']
+    },
+
+    {
+        name : 'Oak',
+        type : 'gltfModel',
+        path : ['./models/trees/oak.glb']
+    },
+
+    {
+        name : 'Spruce',
+        type : 'gltfModel',
+        path : ['./models/trees/spruce.glb']
+    },
+]
