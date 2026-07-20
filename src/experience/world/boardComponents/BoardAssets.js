f3a7733c18539eecde00f5d9c7edc83ee5a8b246
Marco Ferioli <m.ferioli@campus.unimib.it>
2026-07-20 18:38:08 +0200

    Estratto file di confiugrazione degli asset che ora non sono più codificati da una lunga catena di if-els

--- src/experience/world/boardComponents/BoardAssets.js
+++ src/experience/world/boardComponents/BoardAssets.js
@@ -2,6 +2,7 @@ import * as THREE from 'three'
 import Experience from '../../Experience'
 import Tree from './assetsClass/Tree'
 import Rock from './assetsClass/Rock'
+import AssetsConfiguration from './assetsClass/AssetsConfiguration'
 
 export default class BoardAssets {
     constructor(boardPlane, assetMap, heightMap, assetResolution, numberOfAssets) {
@@ -21,67 +22,101 @@ export default class BoardAssets {
             oak:20,
         }
 
+        this.assetConfig = AssetsConfiguration
+        this.setupAssets()
         this.setup()
     }
 
+    setupAssets(){
+        this.assets = {}
+        this.assetConfig.forEach(config => {
+            config.assets.forEach(assetConfig => {
+                if (!this.assets[assetConfig.name]) {
+                    if (assetConfig.class === Tree) {
+                        this.assets[assetConfig.name] = new assetConfig.class(
+                            this.numberOfAssets,
+                            this.boardPlane,
+                            assetConfig.species,
+                            assetConfig.scale
+                        )
+                    } else {
+                        this.assets[assetConfig.name] = new assetConfig.class(
+                            this.numberOfAssets,
+                            this.boardPlane,
+                            assetConfig.scale
+                        )
+                    }
+                }
+            })
+        })
+    }
+
     setup() {
-
-        this.spruceTree = new Tree(this.numberOfAssets, this.boardPlane, "spruce", 2)
-        this.oakTree = new Tree(this.numberOfAssets, this.boardPlane, "oak", 1)
-        this.rock = new Rock(this.numberOfAssets, this.boardPlane, 5)
-
         for(let y = 0; y < this.assetMap.height; y++) {
-
             for(let x = 0; x < this.assetMap.width; x++) {
-
-
                 const index = y * this.assetMap.width + x
 
-
                 if(this.assetMap.data[index] === 1) {
-
-
-                    // posizione iniziale nella heightmap
                     const hx = x * this.assetResolution
                     const hy = y * this.assetResolution
 
-                    const worldX =
-                        -this.boardPlane.width / 2 +
-                        x * (this.boardPlane.width / this.assetMap.width) + Math.random() * 0.4 - 0.2
+                    const worldX = -this.boardPlane.width / 2 +
+                        x * (this.boardPlane.width / this.assetMap.width) + 
+                        Math.random() * 0.4 - 0.2
 
+                    const worldZ = this.boardPlane.height / 2 -
+                        y * (this.boardPlane.height / this.assetMap.height) + 
+                        Math.random() * 0.4 - 0.2
 
-                    const worldZ =
-                        this.boardPlane.height / 2 -
-                        y * (this.boardPlane.height / this.assetMap.height) + Math.random() * 0.4 - 0.2
-
-
-                    const hNormal = this.interpolateHeight(hx,hy) 
-                    const worldY =
-                        this.getWorldHeight(
-                            hNormal
-                        )
+                    const hNormal = this.interpolateHeight(hx, hy)
+                    const worldY = this.getWorldHeight(hNormal)
                     
-                    if (worldY < this.heightScale * 0.1)
-                        this.oakTree.spawn(
+                    const selectedAsset = this.selectAssetForHeight(worldY)
+                    if (selectedAsset) {
+                        this.assets[selectedAsset.name].spawn(
                             worldX,
-                            worldY,
+                            worldY + selectedAsset.positionOffset,
                             worldZ
                         )
-                    else if (worldY > this.heightScale * 0.1 && worldY < this.heightScale * 0.8)
-                        this.spruceTree.spawn(
-                            worldX,
-                            worldY - 0.5,
-                            worldZ
-                        )
-                    else if (worldY >= this.heightScale * 0.8)
-                        this.rock.spawn(
-                            worldX,
-                            worldY -0.2,
-                            worldZ
-                        )
+                    }
                 }
             }
         }
+    }
+
+    selectAssetForHeight(height) {
+
+        const heightConfig = this.assetConfig.find(config =>
+            height >= config.heightRange.min * this.heightScale && 
+            height < config.heightRange.max * this.heightScale
+        )
+
+        if (!heightConfig || heightConfig.assets.length === 0) return null
+
+        if (heightConfig.assets.length === 1) {
+            return heightConfig.assets[0]
+        }
+
+        return this.selectByProbability(heightConfig.assets)
+    }
+
+    selectByProbability(assets) {
+
+        const totalProbability = assets.reduce((sum, a) => sum + a.probability, 0)
+        const normalizedAssets = assets.map(a => ({
+            ...a,
+            normalizedProb: a.probability / totalProbability
+        }))
+
+        let random = Math.random()
+        for (const asset of normalizedAssets) {
+            if (random < asset.normalizedProb) {
+                return asset
+            }
+            random -= asset.normalizedProb
+        }
+
+        return assets[0] 
     }
 
     interpolateHeight(startX, startY) {
